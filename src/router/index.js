c65ca8b3febd26c56633a1b9db795d88e23bc6d6
luckman19980301 <luckman19980301@gmail.com>
2018-09-27 15:10:24 +0100

    Add nested routes for browsing component

--- src/router/index.js
+++ src/router/index.js
@@ -3,25 +3,61 @@ import Router from 'vue-router';
 
 import HomePage from '../home/HomePage.vue';
 import RobotBuilder from '../build/RobotBuilder.vue';
+
+import BrowseParts from '../parts/BrowseParts.vue';
+import RobotHeads from '../parts/RobotHeads.vue';
+import RobotArms from '../parts/RobotArms.vue';
+import RobotTorsos from '../parts/RobotTorsos.vue';
+import RobotBases from '../parts/RobotBases.vue';
+
 import PartInfo from '../parts/PartInfo.vue';
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [{
-    path: '/',
-    name: 'Home',
-    component: HomePage,
-  },
-  {
-    path: '/build',
-    name: 'Build',
-    component: RobotBuilder,
-  },
-  {
-    path: '/parts/:partType/:id',
-    name: 'Parts',
-    component: PartInfo,
-    props: true,
-  }],
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: HomePage,
+    },
+    {
+      path: '/build',
+      name: 'Build',
+      component: RobotBuilder,
+    },
+    {
+      path: '/parts/browse',
+      name: 'BrowseParts',
+      component: BrowseParts,
+      children: [
+        {
+          path: 'heads',
+          name: 'BrowseHeads',
+          component: RobotHeads,
+        },
+        {
+          path: 'arms',
+          name: 'BrowseArms',
+          component: RobotArms,
+        },
+        {
+          path: 'torsos',
+          name: 'BrowseTorsos',
+          component: RobotTorsos,
+        },
+        {
+          path: 'bases',
+          name: 'BrowseBases',
+          component: RobotBases,
+        },
+      ],
+    },
+    {
+      path: '/parts/:partType/:id',
+      name: 'Parts',
+      component: PartInfo,
+      props: true,
+    },
+  ],
 });
