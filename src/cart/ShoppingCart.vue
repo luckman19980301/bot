41563f1d79b18c4ee122fc00347be90fd008201a
luckman19980301 <luckman19980301@gmail.com>
2018-10-03 17:33:44 +0100

    Update getter to reference namespaced module in store

--- src/cart/ShoppingCart.vue
+++ src/cart/ShoppingCart.vue
@@ -57,7 +57,7 @@ export default {
       return this.$store.state.robots.cart;
     },
     cartSaleItems() {
-      return this.$store.getters.cartSaleItems;
+      return this.$store.getters['robots/cartSaleItems'];
     },
   },
 };
