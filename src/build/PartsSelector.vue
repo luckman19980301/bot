a3cb851f60366d7c91eebbb13165d9a7f28a0d59
luckman19980301 <luckman19980301@gmail.com>
2018-10-04 11:53:13 +0100

    Move directive to global scope

--- src/build/PartsSelector.vue
+++ src/build/PartsSelector.vue
@@ -24,8 +24,6 @@
 </template>
 
 <script>
-import pinDirective from '../shared/pin-directive';
-
 function getPreviousValidIndex(index, length) {
   const deprecatedIndex = index - 1;
   return deprecatedIndex < 0 ? length - 1 : deprecatedIndex;
@@ -37,9 +35,6 @@ function getNextValidIndex(index, length) {
 }
 
 export default {
-  directives: {
-    pin: pinDirective,
-  },
   props: {
     parts: {
       type: Array,
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+import pinDirective from './shared/pin-directive';
+
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+Vue.directive('pin', pinDirective);
 
 new Vue({
   render: h => h(App),
