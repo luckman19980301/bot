1bb308034da90cfd82546f00664989caf7575d5b
luckman19980301 <luckman19980301@gmail.com>
2018-10-05 14:03:07 +0100

    Unit tests for the PartsSelector component

--- src/build/__tests__/PartsSelector.spec.js
+++ src/build/__tests__/PartsSelector.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils';
+import { mount, shallowMount, createLocalVue, RouterLinkStub } from '@vue/test-utils';
 import VueRouter from 'vue-router';
 import pinDirective from '../../shared/pin-directive';
 import PartsSelector from '../PartsSelector.vue';
@@ -8,16 +8,37 @@ localVue.use(VueRouter);
 localVue.directive('pin', pinDirective);
 
 describe('PartsSelector.vue', () => {
+  const requiredProps = {
+    position: 'top',
+    parts: ['heads', 'arms'],
+  };
+
   it('renders props.msg when passed', () => {
-    const position = 'top';
-    const parts = ['heads', 'arms'];
     const wrapper = shallowMount(PartsSelector, {
       localVue,
       propsData: {
-        position,
-        parts,
+        ...requiredProps,
       },
     });
     expect(wrapper.contains('button')).toBe(true);
+  });
+
+  it('returns the correct route path with props.to', () => {
+    const wrapper = mount(PartsSelector, {
+      localVue,
+      propsData: {
+        ...requiredProps,
+      },
+      stubs: {
+        RouterLink: RouterLinkStub,
+      },
+    });
+    expect(wrapper.find(RouterLinkStub).props().to).toEqual({
+      name: 'Parts',
+      params: {
+        id: undefined,
+        partType: undefined,
+      },
+    });
   });
 });
